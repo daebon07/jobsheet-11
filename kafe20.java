986666fe8fd0d5d1f5af6c02e9c4507cc0514827
daebon07 <muhammadshabran25@gmail.com>
2024-11-21 13:43:46 +0700

    percobaan 1

---
+++ kafe20.java
@@ -0,0 +1,17 @@
+public class kafe20 {
+
+public static void menu (){
+    System.out.println("==== MENU RESTO KAFE ====");
+    System.out.println("1. kopi hitam - Rp 15,000");
+    System.out.println("2. cappuccino - Rp 20,000");
+    System.out.println("3. latte - Rp 22,000");
+    System.out.println("4. teh tarik - Rp 12,000");
+    System.out.println("5. roti bakat - Rp 10,000");
+    System.out.println("6. mie goreng - Rp 18,000");
+    System.out.println("=========================");
+    System.out.println("silahkan pilih menu yang anda inginkan");
+}
+public static void main(String[] args) {
+    menu();
+}
+}
