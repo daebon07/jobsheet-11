e7c5ddd136112af8465403225ff491f5bbece79e
daebon07 <muhammadshabran25@gmail.com>
2024-11-21 14:21:13 +0700

    percobaan 2

--- kafe20.java
+++ kafe20.java
@@ -1,6 +1,11 @@
 public class kafe20 {
+public static void menu( String namapelanggan, boolean ismember) {
+    System.out.println("selamat datang " + namapelanggan + " !");
 
-public static void main(String[] args) {
+    if (ismember){
+        System.out.println("anda adalah member dapatkan diskon 10% untuk setiap pembelian");
+    }
+
     System.out.println("==== MENU RESTO KAFE ====");
     System.out.println("1. kopi hitam - Rp 15,000");
     System.out.println("2. cappuccino - Rp 20,000");
@@ -10,5 +15,9 @@ public static void main(String[] args) {
     System.out.println("6. mie goreng - Rp 18,000");
     System.out.println("=========================");
     System.out.println("silahkan pilih menu yang anda inginkan");
+    
+}
+public static void main(String[] args) {
+   menu("andi",true);  
 }
 }
