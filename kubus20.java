d3d454c77111de732ddab5bd913232e735695749
daebon07 <muhammadshabran25@gmail.com>
2024-11-21 19:03:19 +0700

    tugas 1

---
+++ kubus20.java
@@ -0,0 +1,28 @@
+public class kubus20 {
+   
+        
+        public static int hitungVolume(int sisi) {
+            return sisi * sisi * sisi; 
+        }
+    
+       
+        public static int hitungLuasPermukaan(int sisi) {
+            return 6 * (sisi * sisi); 
+        }
+    
+        public static void main(String[] args) {
+
+            int sisi = 5; 
+    
+           
+            int volume = hitungVolume(sisi);
+            int luasPermukaan = hitungLuasPermukaan(sisi);
+    
+            
+            System.out.println("Sisi kubus: " + sisi);
+            System.out.println("Volume kubus: " + volume);
+            System.out.println("Luas permukaan kubus: " + luasPermukaan);
+        }
+    
+    
+}
--- percobaan6_20.java
+++ percobaan6_20.java
@@ -1,31 +1,19 @@
 import java.util.Scanner;
 
 public class percobaan6_20 {
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+         int p,l,t,L,vol;
+         System.out.println("masukan panjang");
+         p = input.nextInt();
+         System.out.println("masukan lebar");
+         l = input.nextInt();
+         System.out.println("masukan tinggi");
+         t = input.nextInt();
 
-static int hitungluas (int pjg,int lb){
-    int luas = pjg*lb;
-    return luas;
+         L=p*l;
+         System.out.println("luas persegi panjang adalah " + L);
+         vol= p*l*t;
+         System.out.println("volume persgi panjang adalah "+vol);
 }
-static int hitungvolume (int tg,int a,int b){
-    int vol = tg*a*b;
-    return vol;
-}
-
-public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int p,l,t,L,vol;
-    System.out.println("masukan panjang");
-    p = input.nextInt();
-    System.out.println("masukan lebar");
-    l = input.nextInt();
-    System.out.println("masukan tinggi");
-    t = input.nextInt();
-
-    L=hitungluas(p,l);
-    System.out.println("luas persegi panjang adalah " + L);
-    vol= hitungvolume(p,l,t);
-    System.out.println("volume persgi panjang adalah "+vol);
-}
-
-
 }
